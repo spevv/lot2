cd385aab9521978e10413f75d62673ab920059dd
Alexandr Spivak <spivak91@gmail.com>
2015-06-15 16:27:25 +0300

    Изминения страницы login

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -17,6 +17,8 @@ $this->params['breadcrumbs'][] = $this->title;
         echo '<div class="alert alert-danger">'.Yii::$app->getSession()->getFlash('error').'</div>';
     }
 ?>
+
+Для авторизации выберите другую соцсеть
 <!--
 <p class="lead">Do you already have an account on one of these sites? Click the logo to log in with it here:</p>
 <?php echo \nodge\eauth\Widget::widget(array('action' => 'site/login')); ?>
