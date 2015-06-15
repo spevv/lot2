3a1969eaa685e2666687e820d6b703ccab023c54
Alexandr Spivak <spivak91@gmail.com>
2015-06-15 16:49:01 +0300

    Изминения вида login

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -6,8 +6,7 @@ use yii\bootstrap\ActiveForm;
 /* @var $form yii\bootstrap\ActiveForm */
 /* @var $model \common\models\LoginForm */
 
-$this->title = 'Login';
-$this->params['breadcrumbs'][] = $this->title;
+$this->title = 'Вход';
 ?>
 <div class="site-login">
     <h1><?= Html::encode($this->title) ?></h1>
@@ -18,44 +17,14 @@ $this->params['breadcrumbs'][] = $this->title;
     }
 ?>
 
-
-<p class="lead">Do you already have an account on one of these sites? Click the logo to log in with it here:</p>
-<?php echo \nodge\eauth\Widget::widget(array('action' => 'site/login')); ?>
-
 	
     <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
-                <?= $form->field($model, 'username') ?>
-                <?= $form->field($model, 'password')->passwordInput() ?>
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
-                <div style="color:#999;margin:1em 0">
-                    If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
-                </div>
-                <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                </div>
-            <?php ActiveForm::end(); ?>
+        <div class="col-xs-12">
+            
+			<p class="lead">Войдите через соцсеть:</p>
+			<?php echo \nodge\eauth\Widget::widget(array('action' => 'site/login')); ?>
+            
         </div>
     </div>
-    
-    	
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
-                <?= $form->field($model, 'username') ?>
-                <?= $form->field($model, 'password')->passwordInput() ?>
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
-                <div style="color:#999;margin:1em 0">
-                    If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
-                </div>
-                <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                </div>
-            <?php ActiveForm::end(); ?>
-        </div>
-    </div>
-
-
 
 </div>
